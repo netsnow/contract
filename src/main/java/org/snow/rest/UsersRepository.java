579a2982aebf4071968f5f31e9dd95edb17a38ff
baixue <bai.xue@me.com>
2017-03-28 10:36:31 +0800

    user commit

--- src/main/java/org/snow/rest/UsersRepository.java
+++ src/main/java/org/snow/rest/UsersRepository.java
@@ -1,15 +1,14 @@
 package org.snow.rest;
 
+import org.snow.model.security.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.snow.model.security.User;
-
 import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "users", path = "/users")
 public interface UsersRepository extends PagingAndSortingRepository<User, Long> {
+    @Query("select u from User u where u.username = ?1")
     List<User> findByUsername(@Param("name") String name);
 }
